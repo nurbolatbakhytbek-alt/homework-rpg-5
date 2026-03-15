802761e7b6f825408e7f9d22485d45f084082771
nurbolatb0903-arch <nurbolat.b0903@icloud.com>
2026-03-16 00:20:06 +0500

    Decorator: implement BasicAttack

--- src/com/narxoz/rpg/decorator/BasicAttack.java
+++ src/com/narxoz/rpg/decorator/BasicAttack.java
@@ -1,29 +1,14 @@
 package com.narxoz.rpg.decorator;
 
 public class BasicAttack implements AttackAction {
-    private final String actionName;
-    private final int baseDamage;
 
-    public BasicAttack(String actionName, int baseDamage) {
-        this.actionName = actionName;
-        this.baseDamage = baseDamage;
+    @Override
+    public int execute() {
+        return 10;
     }
 
     @Override
-    public String getActionName() {
-        // TODO: Return the base name of the action.
-        return actionName;
-    }
-
-    @Override
-    public int getDamage() {
-        // TODO: Return the base damage before decorators modify it.
-        return baseDamage;
-    }
-
-    @Override
-    public String getEffectSummary() {
-        // TODO: Decide how a plain attack should describe its effects.
-        return "TODO";
+    public String getDescription() {
+        return "Basic attack";
     }
 }
